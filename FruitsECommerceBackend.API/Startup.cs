9d931c3d0b36b8eb75240c166291a31568598035
hoang.tran-nguyen <hoang.tran-nguyen@soget.fr>
2021-11-28 23:18:02 +0700

    Add missing files in project EFruitsCommerce.API

---
+++ FruitsECommerceBackend.API/Startup.cs
@@ -0,0 +1,53 @@
+namespace FruitsECommerceBackend.API
+{
+    /// <summary>
+    /// Startup class.
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Configuration property.
+        /// </summary>
+        private IConfiguration _configuration { get; }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="configuration"></param>
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        /// </summary>
+        /// <param name="services"></param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { 
+                endpoints.MapControllers(); 
+            });
+        }
+    }
+}
