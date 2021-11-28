51e151bcd2046463b6ee39faaddc27f8f26f55f8
hoang.tran-nguyen <hoang.tran-nguyen@soget.fr>
2021-11-28 21:43:11 +0700

    Rearrange solutions

---
+++ FruitsECommerceBackend.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitsECommerceBackend.Infrastructure.Data
+{
+    internal class ApplicationDbContext
+    {
+    }
+}
